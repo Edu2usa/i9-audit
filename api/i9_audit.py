ac05686c8f8203f53f32fc6befba9cb64eb0b041
Edu2usa <edu2usa@live.com>
2026-04-26 18:36:47 -0400

    Restore i9 app data access

--- api/i9_audit.py
+++ api/i9_audit.py
@@ -16,8 +16,8 @@ DEFAULT_SUPABASE_URL = "https://pfknmvfrsizsdvxknjmm.supabase.co"
 DEFAULT_SUPABASE_KEY = (
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
     ".eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBma25tdmZyc2l6c2R2eGtuam1tIiwic"
-    "m9sZSI6ImFub24iLCJpYXQiOjE3NzI4MjM1NjgsImV4cCI6MjA4ODM5OTU2OH0"
-    ".HFwO-IcBFdqkU6CITuDKg8jMCLbGsQN0VrU4pgiXJfs"
+    "m9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjgyMzU2OCwiZXhwIjoyMDg4Mzk5"
+    "NTY4fQ.rtOZxA1czAbun6arMxUyUaT3NVlqMXBSZaQZ94O8LVc"
 )
 
 
@@ -27,7 +27,12 @@ def _looks_like_jwt(value):
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL") or DEFAULT_SUPABASE_URL
 _env_supabase_key = os.environ.get("SUPABASE_KEY", "").strip()
-SUPABASE_KEY = _env_supabase_key if _looks_like_jwt(_env_supabase_key) else DEFAULT_SUPABASE_KEY
+_env_service_role_key = os.environ.get("SUPABASE_SERVICE_ROLE_KEY", "").strip()
+SUPABASE_KEY = (
+    _env_service_role_key if _looks_like_jwt(_env_service_role_key)
+    else _env_supabase_key if _looks_like_jwt(_env_supabase_key)
+    else DEFAULT_SUPABASE_KEY
+)
 TABLE = "employees_i9"
 
 app = Flask(__name__)
